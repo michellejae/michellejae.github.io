885028d2c23b132571ffe729b6084bc76e7b902a
michelle moyer <moyer.mj@gmail.com>
2018-01-03 18:27:23 -1000

    colors working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -8,25 +8,56 @@ const canvasSection = document.createElement('div');
 canvasSection.id = 'canvas';
 document.body.appendChild(canvasSection);
 
-function createGrid(x,y,section){
+let store;
+
+function swatch(cell) {
+  let randomColor = getRandomColor();
+  cell.style.backgroundColor = randomColor;
+
+  let swatchClick = cell.addEventListener("click", function () {
+    store = cell.style.backgroundColor
+    console.log('color', store)
+  })
+}
+
+function canvas(cell) {
+  let canvasColor = cell.addEventListener("click", function () {
+    cell.style.backgroundColor = store
+  })
+
+}
+
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
+
+function createGrid(x, y, section, choice) {
 
   let container = document.createElement('div');
   container.className = 'container';
 
-  for(let i = 0; i < x; i++){
+  for (let i = 0; i < x; i++) {
     let row = document.createElement('div');
     row.className = 'row';
     container.appendChild(row);
     row.innerHTML;
 
-      for(let j = 0; j < y; j++){
-        let cell = document.createElement('div');
-        cell.className = 'cell';
-        row.appendChild(cell);
-      
-      }
+    for (let j = 0; j < y; j++) {
+      let cell = document.createElement('div');
+      cell.className = 'cell';
+      row.appendChild(cell);
+      choice(cell);
+
+    }
   }
   section.appendChild(container)
 }
-createGrid(10,10,swatchSection);
-createGrid(20,20,canvasSection);
+createGrid(10, 10, swatchSection, swatch);
+createGrid(20, 20, canvasSection, canvas);
